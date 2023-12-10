f32e59429f4098829ca85287ef003350bd20df05
yasa16 <yasasminikeshala@gmail.com>
2023-12-10 13:45:24 +0530

    Change commit port no

--- commit-service/index.js
+++ commit-service/index.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const port = process.env.PORT || 3004;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Commit service app listening on port ${port}`)
